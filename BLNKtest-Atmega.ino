cf9ec274b1a4d27ae5235f28456bb50b5883e950
Ivy Matobori <62935803+i3v3y3@users.noreply.github.com>
2024-06-10 17:52:03 +0300

    Update BLNKtest-Atmega.ino

--- BLNKtest-Atmega.ino
+++ BLNKtest-Atmega.ino
@@ -1,12 +1,11 @@
 // Here you will test whether the system you just designed will work.
 // The code lights an LED for 2 seconds and turns it off for 10 seconds.
 
+
 void setup() {
  // seting up  the IO pins used and whether they are outputs or inputs.
-
 pinMode (4,OUTPUT);
 }
-
 void loop() {
   // putthe main code here, to run repeatedly:
  digitalWrite(9,HIGH);
